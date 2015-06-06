71353d735c735b34c87bcb6ada7c1fc5bf35c283
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-06-06 10:42:55 +0300

    no message

--- plugin.rb
+++ plugin.rb
@@ -1,1 +1,5 @@
-#
+# name: restricted-attachments
+# about: The plugin allows to restrict access to attachments to concrete user groups.
+# version: 1.0.0
+# authors: Dmitry Fedyuk
+# url: http://discourse.pro/t/33
