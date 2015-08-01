496db294e7fa055191830278b5349a3f2d12ed12
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-08-01 07:06:35 +0300

    no message

---
+++ app/models/df/restrict_files/access_list_type.rb
@@ -0,0 +1,17 @@
+module ::Df::RestrictFiles
+	require_dependency 'enum_site_setting'
+	class AccessListType < EnumSiteSetting
+		def self.valid_value?(val)
+			val.blank? or values.any? { |v| v[:value] == val.to_s }
+		end
+		def self.values
+			@values ||= [
+				{name: 'whitelist', value: 'whitelist'},
+				{name: 'blacklist', value: 'blacklist'}
+			]
+		end
+		def self.translate_names?
+			true
+		end
+	end
+end
